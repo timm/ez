1cd634910816447552d2591228e7f90eca0168ac
timm <timm@ieee.org>
2026-01-30 00:23:11 -0500

    as

--- ez.py
+++ ez.py
@@ -10,28 +10,27 @@ Options:
    -l leaf=2    Min examples in leaf of tree
    -p p=2       Distance coeffecient
    -s seed=1    Random number seed
-   -S Show=30   Tree display width 
-"""
+   -S Show=30   Tree display width"""
 from math import log,exp,sqrt
 import re,sys,random,traceback
 BIG=1e32
 
 #-------------------------------------------------------------------------------
 # Create
-def COL(at=0,txt=" "): return obj(txt=txt, at=at, n=0, goal=txt[-1]!="-")
-def NUM(**d):          return obj(it=NUM, **COL(**d), mu=0, m2=0, lo={})
-def SYM(**d):          return obj(it=SYM, **COL(**d), has={})
-def DATA(items):
-  items = iter(items)
-  return adds(items, obj(it=DATA, n=0, rows=[], cols=COLS(next(items))))
+def COL(at=0,txt=" "): return (NUM if txt[0].isupper() else SYM)(at=at,txt=txt)
+def NUM(**d): return OBJ(it=NUM, **d, n=0, mu=0, m2=0, goal=txt[-1]!="-")
+def SYM(**d): return OBJ(it=SYM, **d, n=0, has={})
+
+def DATA(s="", items=[]):
+  return adds(items, OBJ(it=DATA, txt=s, rows=[], cols=None))
 
 def COLS(names):
-  cols= [(NUM if s[0].isupper() else SYM)(at=n,txt=s) for n,s in enumerate(names)]
-  return obj(it=COLS, names=names, all=cols,
-            x= [c for c in cols if c.txt[-1] not in "-+X"],
-            y= [c for c in cols if c.txt[-1]     in "-+" ])
+  cols= [COL(at=n,txt=s) for n,s in enumerate(names)]
+  return OBJ(it=COLS, names=names, all=cols,
+             x= [c for c in cols if c.txt[-1] not in "-+!X"],
+             y= [c for c in cols if c.txt[-1]     in "-+!" ])
 
-def clone(data, rows=None): return DATA([data.cols.names] + (rows or []))
+def clone(data, rows=[]): return DATA(items=[data.cols.names] + rows)
 
 #-------------------------------------------------------------------------------
 # Update
@@ -39,12 +38,11 @@ def adds(items, it=None):
   it = it or NUM(); [add(it,item) for item in items]; return it
 
 def add(i,v):
-  if v != "?":
+  if DATA is i.it : i.rows += [[add(c,v[c.at]) for c in i.cols.all]]
+  elif v != "?":
     i.n += 1
-    if   DATA is i.it : i.rows += [[add(c,v[c.at]) for c in i.cols.all]]
-    elif  SYM is i.it : i.has[v] = 1 + i.has.get(v,0)
-    elif  NUM is i.it : d = v - i.mu; i.mu += d/i.n; i.m2 += d*(v - i.mu)
-    else: raise TypeError(f"add error on '{type(i)}'")
+    if SYM is i.it : i.has[v] = 1 + i.has.get(v,0)
+    if NUM is i.it : d = v - i.mu; i.mu += d/i.n; i.m2 += d*(v - i.mu)
   return v
 
 #-------------------------------------------------------------------------------
@@ -61,17 +59,8 @@ def sd(num):     return 0 if num.n < 2 else sqrt(num.m2 / (num.n - 1))
 def ent(sym):    return -sum(p*log(p,2) for n in sym.has.values() if (p:=n/sym.n)>0)
 
 def z(num,v):    return (v -  num.mu) / (sd(num) + 1/BIG)
-def norm(num,v): return 1 / (1 + exp( -1.7 * max(-3, min(3, z(num,v)))))
-def bucket(col,v):
-  if v=="?" or  SYM is col.it: return v
-  b = int(the.bins * norm(col,v))
-  col.lo[b] = min(v, col.lo.get(b,v))
-  return b
-
-def tekcub(col,b):
-  if SYM is col.it: return b,b
-  # Find next smallest key > b for the upper bound, else BIG
-  return col.lo.get(b,-BIG), min((v for k,v in col.lo.items() if k>b),default=BIG)
+def norm(num,z): return 1 / (1 + exp( -1.7 * z))
+def bucket(num,v): return  int(the.bins * norm(num, clip(z(num,v), -3,3)))
 
 #-------------------------------------------------------------------------------
 # distance
@@ -94,75 +83,28 @@ def aha(col,u,v):
   v = v if v != "?" else (0 if u>0.5 else 1)
   return abs(u - v)
 
-def furthest(*args): return nearest(*args,max)
-def nearest(data,row,rows,fn=min): return fn(rows,key=lambda r:distx(data,row,r))
-
-#------------------------------------------------------------------------------
-# tree
-def Tree(data, uses=None):
-  uses = uses or set()
-  def bestcut(rows):
-    d={}
-    for r in rows:
-      y = disty(data,r)
-      for col in data.cols.x:
-        k = (col.at, bucket(col, r[col.at]))
-        if k not in d: d[k] = NUM()
-        add(d[k], y)
-    return min(d.items(), key=lambda x: score(x[1]), default=None)
-
-  def grow(rows):
-    at, b, kids = None, None, {}   
-    if len(rows) > the.leaf*2:
-      if cut := bestcut(rows):
-        ((at,b), _) = cut
-        ok, no = [], []
-        for r in rows:
-          (ok if b == bucket(data.cols.all[at], r[at]) else no).append(r)
-        if ok and no:
-          uses.add(at)
-          kids = {True:grow(ok), False:grow(no)}
-    return obj(root=data, kids=kids, at=at, bucket=b,
-               x= mids(clone(data,rows)),
-               y= adds(disty(data,row) for row in rows))
-
-  return grow(data.rows), uses
-
-def treeLeaf(tree, row):
-  if not tree.kids: return tree
-  col = tree.root.cols.all[tree.at]
-  what = bucket(col, row[col.at]) == tree.bucket
-  return treeLeaf(tree.kids[what], row)
-
-def treeShow(t):
-  def show(n, lvl, pre):
-    g = [n.x[c.at] for c in n.root.cols.y]
-    print(f"{('| '*(lvl-1)+pre):{the.Show}}: {o(n.y.mu):6} : {n.y.n:4} : {o(g)}")
-    for k in sorted(n.kids or {}, reverse=True):
-      c, b = n.root.cols.all[n.at], n.bucket
-      if SYM is c.it: s = f"{c.txt} {'==' if k else '!='} {b}"
-      else:
-        lo, hi = tekcub(c, b)
-        if k: s=f"{c.txt}<{o(hi)}" if lo==-BIG else (f"{c.txt}>={o(lo)}"
-                if hi==BIG else f"{o(lo)}<={c.txt}<{o(hi)}")
-        else: s=f"{c.txt}>={o(hi)}" if lo==-BIG else (f"{c.txt}<{o(lo)}"
-                if hi==BIG else f"{c.txt}<{o(lo)} or >={o(hi)}")
-      show(n.kids[k], lvl+1, s)
-  ys = ', '.join([y.txt for y in t.root.cols.y])
-  print(f"{'':{the.Show}}   Score      N   [{ys}]"); show(t, 0, "")
+def furthest(*args): return around(*args)[-1]
+def nearest(*args): return around(*args)[0]
+def around(data,row,rows): return sorted(rows,key=lambda r:distx(data,row,r))
 
 #------------------------------------------------------------------------------
 # lib
+
+def clip(v,lo,hi): max(lo, min(hi, v))
+
+def shuffle(lst): random.shuffle(lst); return lst
+
 def o(t):
   match t:
+    case _ if type(t) is type(o): return t.__doc__
     case dict(): return "{" + " ".join(f":{k} {o(t[k])}" for k in t) + "}"
     case float(): return f"{int(t)}" if int(t) == t else f"{t:.2f}"
     case list(): return "[" + ", ".join(o(x) for x in t) + "]"
     case tuple(): return "(" + ", ".join(o(x) for x in t) + ")"
     case _: return str(t)
 
-class obj(dict):
-  __getattr__, __setattr__, __repr__ = dict.__getitem__, dict.__setitem__, o
+class OBJ(dict):
+  __getattr__,__setattr__,__repr__ = dict.__getitem__,dict.__setitem__,o
 
 def gauss(mu,sd1):
   return mu + 2 * sd1 * (sum(random.random() for _ in range(3)) - 1.5)
@@ -173,26 +115,15 @@ def pick(d,n):
     if (n := n-v) <= 0: break
   return k
 
-def era(items, size=20):
-  cache = []
-  for item in items:
-    cache += [item]
-    if len(cache) > size: yield shuffle(cache); cache=[]
-  if cache: yield shuffle(cache)
-
-def shuffle(lst): random.shuffle(lst); return lst
-
-def coerce(s, BOOL={"true": True, "false": False}):
+def cast(s, BOOL={"true": True, "false": False}):
   try: return int(s)
   except ValueError:
     try: return float(s)
     except ValueError: return BOOL.get(s, s)
 
-def csv(fileName):
-  with open(fileName, encoding="utf-8") as src:
-    for line in src:
-      if line := re.sub(r'\s+', '', line.split("#")[0]): # no whitespace, skip comments
-        yield [coerce(x) for x in line.split(",")]
+def csv(f):
+  with open(f) as file:
+    for s in file: yield [cast(x) for x in s.split(",")]
 
 #-------------------------------------------------------------------------------
 # cli
@@ -204,24 +135,35 @@ def run(f,*args):
 def filename(s): return s
 
 def eg_h():
-  """show help"""
+  "Show help."
   print(__doc__)
   for k, v in globals().items():
-    if k.startswith("eg_"): print(f"  -{k[3:]:<10} {v.__doc__.strip()}")
+    if k.startswith("eg_"): print(f"   -{k[3:]:<12}{v.__doc__.strip()}")
 
-def go__the() : print(the)
-def go_s(n:int)    : the.seed=n; random.seed(n)
-def go__csv(f:filename) : [print(row) for row in list(csv(f))[::40]]
+def eg__the(): 
+  "Show config."
+  print(the)
 
-def go__syms():
-    syms = adds("aaaabbc",SYM()); print(o(x:=ent(syms))); assert abs(1.379-x) < .05
+def eg_s(n:int): 
+  "Set random number seed."
+  the.seed=n; random.seed(n)
 
-def go__nums():
+def eg__csv(f:filename) : 
+  "Example: csv reader."
+  [print(row) for row in list(csv(f))[::40]]
+
+def eg__syms():
+  "Example: SYMs summaries."
+  syms = adds("aaaabbc",SYM()); print(o(x:=ent(syms))); assert abs(1.379-x) < .05
+
+def eg__nums():
+  "Example: NUMs summaries."
   nums = adds(gauss(10, 1) for _ in range(1000))
-  print(obj(mu=nums.mu, sd=sd(nums)))
+  print(OBJ(mu=nums.mu, sd=sd(nums)))
   assert abs(10 - nums.mu) < .05 and abs(1 - sd(nums)) < .05
 
-def go__ys(f:filename):
+def eg__ys(f:filename):
+  ""
   data = DATA(csv(f))
   print(*data.cols.names)
   print(o(mids(data)))
@@ -229,13 +171,15 @@ def go__ys(f:filename):
     print(*row,*[bucket(col,row[col.at]) for col in data.cols.y],
           round(disty(data,row),2))
 
-def go__tree(f:filename):
+def eg__tree(f:filename):
+  ""
   data = DATA(csv(f))
   data1 = clone(data, shuffle(data.rows)[:50])
   tree,_ = Tree(data1)
   treeShow(tree)
 
-def go__test(f:filename):
+def eg__test(f:filename):
+  ""
   data = DATA(csv(filename))
   mid  = len(data.rows)//2
   Y    = lambda r: disty(data,r)
@@ -254,7 +198,7 @@ def go__test(f:filename):
         *filename.split("/")[-2:], sep=" ,")
 
 #------------------------------------------------------------------------------
-the= Obj(**{k: cast(v) for k, v in re.findall(r"-(\w+)\s+\w+=(\S+)", __doc__)})
+the= OBJ(**{k: cast(v) for k, v in re.findall(r"(\S+)=(\S+)", __doc__)})
 random.seed(the.seed)
 
 if __name__ == "__main__":
